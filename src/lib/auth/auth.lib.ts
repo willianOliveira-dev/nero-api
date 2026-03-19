cedfc721ef9b18ec7e7fc8a83d5be2b9eafdc54c
Willian Oliveira <willian.dev.tech@gmail.com>
2026-03-19 20:09:51 -0300

    feat: add better-auth localization support and enhance trusted origins configuration in authentication library

--- src/lib/auth/auth.lib.ts
+++ src/lib/auth/auth.lib.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { openAPI } from 'better-auth/plugins';
+import { localization } from 'better-auth-localization';
 import { env } from '@/config/env.js';
 import { db } from '@/lib/db/connection';
 import * as schema from '@/lib/db/schemas/index.schema';
@@ -8,7 +9,15 @@ import * as schema from '@/lib/db/schemas/index.schema';
 export const auth = betterAuth({
     baseURL: env.BETTER_AUTH_URL,
     secret: env.BETTER_AUTH_SECRET,
-    trustedOrigins: env.ALLOWED_ORIGINS,
+    trustedOrigins: [
+        ...env.ALLOWED_ORIGINS,
+        'nero://',
+        ...(process.env.NODE_ENV !== 'production' ? [
+            'exp://',
+            'exp://**',
+            'exp://192.168.*.*:*/**',
+        ] : []),
+    ],
     emailAndPassword: {
         enabled: true,
     },
@@ -22,5 +31,8 @@ export const auth = betterAuth({
         provider: 'pg',
         schema: schema,
     }),
-    plugins: [openAPI()],
+    plugins: [
+        openAPI(),
+        localization({ defaultLocale: 'pt-BR' })
+    ],
 });
