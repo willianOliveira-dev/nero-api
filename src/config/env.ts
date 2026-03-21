68ce3bc012a32c1876a88b0fa8ab84768c250bb4
Willian Oliveira <willian.dev.tech@gmail.com>
2026-03-21 12:57:57 -0300

    feat: add API documentation with Swagger and Scalar, and centralize environment variable configuration.

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    BASE_URL: z.string().default('http://localhost:8000'),
     CLOUDINARY_CLOUD_NAME: z.string(),
     CLOUDINARY_API_KEY: z.string(),
     CLOUDINARY_API_SECRET: z.string(),
